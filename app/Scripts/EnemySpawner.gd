1992c577ee3d5b119cec9e430f38792196688e5e
Personal <c1jn3grog0@gmail.com>
2023-09-25 10:08:16 -0400

    adds enemy manager, enemy spawning

---
+++ app/Scripts/EnemySpawner.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+onready var _player = get_node("/root/Main/Player")
+onready var _enemy_manager = get_node("/root/Main/EnemyManager")
+onready var _timer: Timer = $Timer
+
+const SPAWN_RADIUS = 350
+const MAX_COUNT = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.start(2)
+
+
+func _on_Timer_timeout():
+	if _enemy_manager.size() < MAX_COUNT:
+		# spawn enemy
+		var enemy = _enemy_manager.spawn_enemy()
+		enemy.position = _player.position + _random_normalized_direction() * SPAWN_RADIUS
+
+
+func _random_normalized_direction() -> Vector2:
+	var angle = rand_range(0, 2 * PI)
+	return Vector2(cos(angle), sin(angle)).normalized()
+	
+	
