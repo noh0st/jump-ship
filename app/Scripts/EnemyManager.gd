1034adc0358a1a82181035353ea4e58522df8040
Personal <c1jn3grog0@gmail.com>
2023-10-02 21:00:24 -0400

    rock enemy spawner

--- app/Scripts/EnemyManager.gd
+++ app/Scripts/EnemyManager.gd
@@ -2,10 +2,37 @@ extends Node
 
 var _enemies = []
 const Enemy: PackedScene = preload("res://Scenes/Enemy.tscn")
+const RockTurtle: PackedScene = preload("res://Scenes/RockTurtle.tscn")
+
+enum Type {
+	ENEMY,
+	ROCK_TURTLE,
+}
+
+# manages pools for each enemy type
 
 # Function to spawn a new enemy
+func spawn(type: int) -> Node:
+	match type:
+		Type.ENEMY:
+			return spawn_enemy()
+		Type.ROCK_TURTLE:
+			return spawn_rock_turtle()
+		_:
+			print("ERROR: type not valid %d" % type)
+			return spawn_enemy()
+
+
 func spawn_enemy() -> Node:
 	var new_enemy: Node = Enemy.instance()
+	_enemies.append(new_enemy)
+	add_child(new_enemy)
+	
+	return new_enemy
+
+
+func spawn_rock_turtle() -> Node:
+	var new_enemy: Node = RockTurtle.instance()
 	_enemies.append(new_enemy)
 	add_child(new_enemy)
 	
@@ -17,7 +44,7 @@ func size() -> int:
 
 
 # Function to remove an enemy
-func remove_enemy(enemy) -> void:
+func remove_enemy(enemy: Node) -> void:
 	if _enemies.has(enemy):
 		enemy.queue_free()
 		_enemies.erase(enemy)
