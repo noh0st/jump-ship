d72c4ca16a7028b1bf5f9df41c1b4637ecd1d8c1
AMIRALI <sharifiamirali3@gmail.com>
2023-10-06 19:25:32 -0400

    Temporary PopUp added

--- app/Scripts/UpgradesHandler.gd
+++ app/Scripts/UpgradesHandler.gd
@@ -3,10 +3,17 @@ extends Node
 export(Array, Resource) var presentUpgrades
 
 func AddAndApplyUpgrade(added_upgrade):
-	presentUpgrades.append(added_upgrade)
-	if added_upgrade.has_method("Upgrade"):
-		added_upgrade.Upgrade()
-		PlayerStats.UpgradedValues()
+	if added_upgrade != null:
+		presentUpgrades.append(added_upgrade)
+		if added_upgrade.has_method("Upgrade"):
+			added_upgrade.Upgrade()
+			PlayerStats.UpgradedValues()
 		
 func _ready():
-	AddAndApplyUpgrade(load("res://Recources/new_resource.tres"))
+
+	$Control/ConfirmationDialog.popup()
+	
+
+
+func _on_ConfirmationDialog_confirmed():
+	AddAndApplyUpgrade($Control/ConfirmationDialog.AssignedUpgrade)
