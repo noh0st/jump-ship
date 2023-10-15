0954df56349ea797a6cee2f4b99f78295581653f
AMIRALI <sharifiamirali3@gmail.com>
2023-10-15 13:20:10 -0400

    UpgradeSystemFontandSpriteAdded

--- app/Scripts/MainMenu.gd
+++ app/Scripts/MainMenu.gd
@@ -2,23 +2,22 @@ extends Control
 
 
 
-func _on_Button_mouse_entered():
-	$HoveredSFX.play()
-
-
-
 func _on_QuitGameButton_pressed():
 	get_tree().quit()
-
+	
 
 func _on_StartGameButton_pressed():
 	$PressedSFX.play()
-	get_tree().change_scene("res://Scenes/Main.tscn")
+	
 
 
 func _on_PressedSFX_finished():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Scenes/Main.tscn")
 
 
 func _on_StartGameButton_mouse_entered():
-	pass # Replace with function body.
+	$HoveredSFX.play()
+
+
+func _on_QuitGameButton_mouse_entered():
+	$HoveredSFX.play()
