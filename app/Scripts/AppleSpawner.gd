57721a187516c003c26d336a59decdbf7619855e
AMIRALI <39832336+abbclas@users.noreply.github.com>
2023-10-15 22:48:01 -0400

    Hollow/apple tree drop (#54)

--- app/Scripts/AppleSpawner.gd
+++ app/Scripts/AppleSpawner.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 onready var _timer = $Timer
 var apple = preload("res://Scenes/Apple.tscn")
-onready var MainYsort = get_node("../YSort")
+onready var MainYsort = self.get_parent().get_parent().get_parent()
 
 func _on_Timer_timeout():
 	var new_apple = apple.instance()
 	MainYsort.add_child(new_apple)
-	new_apple.position = Vector2(rand_range(-1000, 1000),rand_range(-1000, 350))
-	print("spawned applae at" ,new_apple.position)
+	new_apple.position = Vector2(rand_range(get_parent().position.x -60,get_parent().position.x + 60),rand_range(get_parent().position.x -60, get_parent().position.y + 60))
+	print("spawned apple at" ,new_apple.position)
